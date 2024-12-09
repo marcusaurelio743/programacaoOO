54be6a8e45e90ed698ed15420c882849d295bfca
marcus aurelio <marcusaurelio743@gmail.com>
2024-12-09 20:21:06 -0300

    usando os metodos get e set da classe e e instanciando objeto

--- ProgramacaoOO/src/projeto/classes/Aluno.java
+++ ProgramacaoOO/src/projeto/classes/Aluno.java
@@ -1,16 +1,99 @@
 package projeto.classes;
 
 public class Aluno {
-	String nome;
-	Integer idade;
-	String dataNascimento;
-	String registroGeral;
-	String CPF;
-	String nomeMae;
-	String nomePai;
-	String dataMatricula;
-	String nomeEscola;
-	String serieMatriculado;
-	
-	
+	private String nome;
+	private Integer idade;
+	private String dataNascimento;
+	private String registroGeral;
+	private String CPF;
+	private String nomeMae;
+	private String nomePai;
+	private String dataMatricula;
+	private String nomeEscola;
+	private String serieMatriculado;
+
+	public Aluno() {
+
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+
+	public String getCPF() {
+		return CPF;
+	}
+
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+
 }
--- ProgramacaoOO/src/projeto/main/Principal.java
+++ ProgramacaoOO/src/projeto/main/Principal.java
@@ -1,8 +1,20 @@
 package projeto.main;
 
+import projeto.classes.Aluno;
+
 public class Principal {
 
 	public static void main(String[] args) {
+		Aluno aluno1 = new Aluno();
+		aluno1.setNome("jose");
+		aluno1.setDataMatricula("09/12/2024");
+		aluno1.setIdade(30);
+		
+		System.out.println(aluno1.getNome());
+		System.out.println(aluno1.getDataMatricula());
+		System.out.println(aluno1.getIdade());
+		
+		
 		
 
 	}
