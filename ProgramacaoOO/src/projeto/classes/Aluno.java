b28bf19682aa37ab1e0706c50de3b5c6c991442c
marcus aurelio <marcusaurelio743@gmail.com>
2024-12-09 19:56:56 -0300

    criando classes para entendimento da OO Exercicio revisão

---
+++ ProgramacaoOO/src/projeto/classes/Aluno.java
@@ -0,0 +1,16 @@
+package projeto.classes;
+
+public class Aluno {
+	String nome;
+	Integer idade;
+	String dataNascimento;
+	String registroGeral;
+	String CPF;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+	
+	
+}
---
+++ ProgramacaoOO/src/projeto/main/Principal.java
@@ -0,0 +1,10 @@
+package projeto.main;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
